6430c0dad526def4844970c0311a33d98e85f7ee
Krosen11 <larrybarnson@gmail.com>
2015-05-04 12:51:15 -0500

    Score stuff

--- ScoreEngine.c
+++ ScoreEngine.c
@@ -18,6 +18,16 @@
 uint32_t score;
 uint8_t found_score = FALSE;
 UINT reads, writes;
+TopLevelState TopScores =  {
+	0,
+	0,
+	0,
+	0,
+	0,
+	&ScoreEngine_startPressed,
+	0,
+	&ScoreEngine_displayScores,
+};
 
 void ScoreEngine_init(){
 	score = 0;
@@ -25,6 +35,10 @@ void ScoreEngine_init(){
 
 uint32_t ScoreEngine_getScore(void){
 	return score;
+}
+
+void ScoreEngine_startPressed(){
+	
 }
 
 void ScoreEngine_update(uint8_t object){
@@ -55,12 +69,33 @@ void ScoreEngine_displayFinalScore(){
 
 void ScoreEngine_displayScores(){
 	//f_open and f_read/f_write
-	uint32_t index = 0;
+	unsigned char current_score[5];
+	uint8_t index = 0;
+	
+	ST7735_FillScreen(0);
 	Fresult = f_open(&Handle, "scores.txt", FA_READ);
 	Fresult = f_read(&Handle, buffer, 512, &reads);
 	if (Fresult == FR_OK){
-		int i;
-		for (
+		int i = 0;
+		while (buffer[i] != 0){
+			char c = buffer[i];
+			if (c != ','){
+				//digit, so let's add it to a string
+				current_score[index] = c;
+				index++;
+			}
+			else {
+				if (index < 5){
+					//score is lower than 5 digits, so let's fill the back end of this array with 0's
+					for (index; index <= 5; index++){
+						current_score[index] = 0;
+					}
+				}
+				printf("%s\n", current_score);
+				index = 0;
+			}
+			i++;
+		}
 	}
 	else {
 		
--- ScoreEngine.h
+++ ScoreEngine.h
@@ -2,8 +2,11 @@
 #define _SCORE_ENGINE_H_
 
 #include "stdint.h"
+#include "TopLevelState.h"
+extern TopLevelState TopScores;
 void ScoreEngine_init(void);
 void ScoreEngine_update(uint8_t object);
+void ScoreEngine_startPressed(void);
 void ScoreEngine_displayScores(void);
 void ScoreEngine_displayFinalScore(void);
 uint32_t ScoreEngine_getScore(void);
